03bc858c86b09fa45ae56bddf2767db6054d2c20
Alessandro Fontana <extraneo@gmail.com>
2025-03-22 12:13:32 +0100

    Init project

---
+++ web/themes/fokos_theme/js/fokos-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * fokOs behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fokosTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
